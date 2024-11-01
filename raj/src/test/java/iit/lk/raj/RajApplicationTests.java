bd0366873e12a80fab33fa772f912dc2b69e5700
Raj162002 <Ramkumarrajeevan16@gmail.com>
2024-11-01 17:12:41 +0530

    Creating of the git

---
+++ raj/src/test/java/iit/lk/raj/RajApplicationTests.java
@@ -0,0 +1,13 @@
+package iit.lk.raj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
