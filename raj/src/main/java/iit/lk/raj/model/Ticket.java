521533a593cadff1719c34eb601d2d04545ed76b
Raj162002 <Ramkumarrajeevan16@gmail.com>
2024-11-01 19:28:49 +0530

    Made basic things to ticket class but still waiting on creating something like creating with checking the ticket availability

--- raj/src/main/java/iit/lk/raj/model/Ticket.java
+++ raj/src/main/java/iit/lk/raj/model/Ticket.java
@@ -1,5 +1,46 @@
 package iit.lk.raj.model;
 
 public class Ticket {
+    private int ticketId;
+    private String ticketType;
+    private String ticketPrice;
+    private boolean ticketStatus;
 
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(int ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(String ticketType) {
+        this.ticketType = ticketType;
+    }
+
+    public String getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public void setTicketPrice(String ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+
+    public boolean isTicketStatus() {
+        return ticketStatus;
+    }
+
+    public void setTicketStatus(boolean ticketStatus) {
+        this.ticketStatus = ticketStatus;
+    }
+
+    public Ticket(int ticketId, String ticketType, String ticketPrice) {
+        this.ticketId = ticketId;
+        this.ticketType = ticketType;
+        this.ticketPrice = ticketPrice;
+    }
 }
