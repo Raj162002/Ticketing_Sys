4baafd714bb0b2336d1282e0558dfade11df3728
Raj162002 <Ramkumarrajeevan16@gmail.com>
2024-11-07 19:40:26 +0530

    Making of the Customer Class and relationship mapping it with Ticket

--- raj/src/main/java/iit/lk/raj/model/Customer.java
+++ raj/src/main/java/iit/lk/raj/model/Customer.java
@@ -1,4 +1,31 @@
 package iit.lk.raj.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int customerId;
+
+    private String customerName;
+    private String customerEmail; // No email validation here
+    private String customerContactNumber; // Contact number
+
+    // One customer can have multiple tickets
+    @OneToMany(mappedBy = "customer")
+    private List<Ticket> tickets;
 }
