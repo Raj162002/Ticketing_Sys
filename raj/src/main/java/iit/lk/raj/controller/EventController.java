9ad1518ecf363776aac27d7ec2c924667cb33a62
Raj162002 <Ramkumarrajeevan16@gmail.com>
2024-11-09 15:41:46 +0530

    fix: Fix /test and /addEvent endpoints in TestController
    - Resolved issues with the /test and /addEvent endpoints to ensure they work correctly.
    - Successfully integrated functionality to add and retrieve Event objects.
    - Note: Date values should be sent in yyyy-MM-dd format in the request body.

--- raj/src/main/java/iit/lk/raj/controller/EventController.java
+++ raj/src/main/java/iit/lk/raj/controller/EventController.java
@@ -1,8 +1,25 @@
 package iit.lk.raj.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import iit.lk.raj.model.Event;
+import iit.lk.raj.service.EventService;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/event")
+@RestController()
+@RequestMapping(value = "/event")
 public class EventController {
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @PostMapping(value = "/addEvent")
+    public Event addEvent(@RequestBody Event event){
+        return eventService.createEvent(event);
+    }
+    @GetMapping(value = "/test")
+    public String test() {
+        return "API works correctly from Event Controller";
+    }
 
 }
--- raj/src/main/java/iit/lk/raj/model/Event.java
+++ raj/src/main/java/iit/lk/raj/model/Event.java
@@ -27,5 +27,18 @@ public class Event {
     private Date eventTicketClosingDate;
     private boolean eventStatus;
     private long eventTotalTickets;
+    public Event(String eventName, String eventLocation, Date eventDate, String eventStartTime, String eventEndTime, long eventTicketCount, long eventTicketPrice, long eventVIPPrice, Date eventTicketReleaseDate, Date eventTicketClosingDate, long eventTotalTickets) {
+        this.eventName = eventName;
+        this.eventLocation = eventLocation;
+        this.eventDate = eventDate;
+        this.eventStartTime = eventStartTime;
+        this.eventEndTime = eventEndTime;
+        this.eventTicketCount = eventTicketCount;
+        this.eventTicketPrice = eventTicketPrice;
+        this.eventVIPPrice = eventVIPPrice;
+        this.eventTicketReleaseDate = eventTicketReleaseDate;
+        this.eventTicketClosingDate = eventTicketClosingDate;
+        this.eventTotalTickets = eventTotalTickets;
+    }
 
 }
